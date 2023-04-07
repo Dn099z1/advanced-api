2a93ef729cf59504ae178c1c9bc44539e9e726a3
Daniel Angelo <likeezkvl@gmail.com>
2023-04-07 18:13:44 -0300

    correct licenses

--- src/pages/api/licenses.js
+++ src/pages/api/licenses.js
@@ -1,6 +1,6 @@
 export default function hadler(req, res) {
     res.status(200).json([
-        {id: 1, name: AdvancedNetWork, pass:123 },
-        {id: 2, name: AdvancedNetWork2, pass:1234 }
+        {id: 16316, license: "AdvancedNetWorkeiV4cqfE!%A85Dq$GRkFVw$8d$hfke", pass:19108 },
+        {id: 44580, license: "AdvancedNetWork2EmjLs7d^kRi^##KmhsqT3y@7pfkYTy", pass:84730 }
     ])
 }

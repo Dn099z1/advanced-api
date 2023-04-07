aac309a6f763b622cb85b39c75ddbdda9314882c
Daniel Angelo <likeezkvl@gmail.com>
2023-04-07 18:19:57 -0300

    update licenses

--- src/pages/api/licenses.js
+++ src/pages/api/licenses.js
@@ -1,6 +1,13 @@
 export default function hadler(req, res) {
     res.status(200).json([
-        {id: 16316, license: "AdvancedNetWorkeiV4cqfE!%A85Dq$GRkFVw$8d$hfke", pass:19108 },
-        {id: 44580, license: "AdvancedNetWork2EmjLs7d^kRi^##KmhsqT3y@7pfkYTy", pass:84730 }
+    {
+    id: 16316, 
+    name:"AdvancedNetWorkeiV4cqfE!%A85Dq$GRkFVw$8d$hfke", 
+    pass:19108 
+    },
+    {
+    id: 44580, 
+    name: "AdvancedNetWork2EmjLs7d^kRi^##KmhsqT3y@7pfkYTy",
+    pass:84730 }
     ])
 }
